8d75775b0978b575ee5bb7d01e2b873dac6d3ec7
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-06 14:14:30 +0100

    ADD: added html, css and js file. Added the initial for cycle in the js

---
+++ js/app.js
@@ -0,0 +1,18 @@
+console.log("inizio")
+
+//stampare in console i numeri da 1 a 10
+for(let i = 0; i < 100; i++){
+    let num = i + 1
+    console.log(num)
+}
+
+
+//dichiaro le variabili alle quali assegno rispettivamente i valori: num % 3 e num % 5
+
+//stampare la parola "Fizz" al posto dei numeri multipli di 3
+
+
+
+//stampare la parola "Buzz" al posto dei numeri multipli di 5
+
+//stampare la parola "FizzBuzz" al posto dei numeri multipli sia di 3 sia di 5
