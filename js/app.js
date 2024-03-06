c4a600002dd00078c610c379cf7f95471d5c31c4
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-06 15:26:26 +0100

    ADD: addedd the if, else if and else inside the cycle for in the js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
-console.log("inizio")
+console.log("inizio FizzBuzz")
 
 //stampare in console i numeri da 1 a 10
-for(let i = 0; i < 100; i++){
+let n = 100
+
+for(let i = 0; i < n; i++){
     let num = i + 1
-    console.log(num)
+    // console.log(num)
+    
+    //dichiaro le variabili alle quali assegno rispettivamente i valori: num % 3 e num % 5
+    let module3 = num % 3
+    
+    let module5 = num % 5
+
+    
+    if(module3 == 0 && module5 == 0){
+        //stampare la parola "FizzBuzz" al posto dei numeri, se i numeri sono sia multipli di 3 sia multipli di 5
+        console.log("FizzBuzz")
+    }else if(module3 == 0){
+        //stampare la parola "Fizz" al posto dei numeri, se i numeri sono multipli di 3
+        console.log("Fizz")
+    }else if(module5 === 0){
+        //stampare la parola "Buzz" al posto dei numeri, se i numeri sono multipli di 5
+        console.log("Buzz")
+    }else{
+        //stampare normalmente i numeri che non sono né multipli di 3, né multipli di 5
+        console.log(num)
+    }
+    
 }
-
-
-//dichiaro le variabili alle quali assegno rispettivamente i valori: num % 3 e num % 5
-
-//stampare la parola "Fizz" al posto dei numeri multipli di 3
-
-
-
-//stampare la parola "Buzz" al posto dei numeri multipli di 5
-
-//stampare la parola "FizzBuzz" al posto dei numeri multipli sia di 3 sia di 5
